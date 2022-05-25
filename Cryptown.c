fc01cef579f7f8341a01165dabc99f380bf23a53
supervillain419 <90406826+supervillain419@users.noreply.github.com>
2022-05-25 17:10:08 +0300

    Project finished

--- Cryptown.c
+++ Cryptown.c
@@ -8,6 +8,13 @@ void Affine();
 void Permutation();
 void Otp();
 void Vigenere();
+void Hill();
+void hill_encryption();
+void hill_decryption();
+float encrypt[3][1], decrypt[3][1], mes[3][1],a[3][3],c[3][3];
+int c_ex[5][5],rev[3][3],end[3][1],key[3][1];
+char msg[3];
+int i,j,num,det,k,m,x,number,flag=0;
 
 int main() {
     printf("---------- Welcome to Cryptown -----------\n");
@@ -35,7 +42,8 @@ int main() {
         
         break;
     case 4:
-        //Hill();
+        printf("---------------- HILL Cypher -----------------\n");
+        Hill();
         break;
     case 5:
         printf("---------------- Permutation Cypher ---------------\n");
@@ -130,11 +138,6 @@ void Caesar(){
 
     
 }
-
-
-
-/*---------------------- HILL ----------------------*/
-
 
 
 
@@ -465,4 +468,179 @@ void Otp(){
         printf("Some error ocured!");
         break;
     }
+}
+
+
+
+/*---------------------- HILL ----------------------*/
+
+void Hill(){
+    /*
+    Step 1: Get the message 
+    Step 2: Multiply and substruct to find DetK 
+    Step 3: Add 2 lines and 2 rows and ignore the first column and row 
+    Step 4: Use mod26 
+    Step 5: If number is negative add 26
+    Step 6: Find the 1/det (det ^-1) -----> i x det = 1 mod 26
+    Step 7: Then multiply k^-1 with the maxtrix and then mod 26
+    Step 8: Multiply inversed matrix with the message
+    */
+    int cho;
+    int i, j,k;
+     do{
+        printf("Choose an option!\n1.Encryption\n2.Decryption\n");
+        printf("---------------------\n");
+        scanf("%d",&cho);
+    }while(cho<0 && cho>3);
+     do{
+        printf("Enter 3x3 matrix for key (It should be inversible):\n");
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++) {
+                scanf("%f", &a[i][j]);
+                c[i][j] = a[i][j];
+            }
+        }
+        det=(c[0][0]*((c[1][1]*c[2][2])-(c[2][1] * c[1][2]))) -
+        (c[0][1] * (c[1][0]*c[2][2] - c[2][0]*c[1][2])) +
+        (c[0][2]*(c[1][0] * c[2][1] - c[2][0]* c[1][1]));
+        det = det % 26;
+        //printf("\n one is: %f, two is %f, three is : %f",one,two,three);
+        if (det==0){
+        printf("It's not inversible!!!\n");
+        }
+    } while (det==0);
+    printf("\nEnter a 3 letter string: ");
+    scanf("%3s", msg);
+    for(i = 0; i < 3; i++){
+        mes[i][0] = msg[i] - 97; //!!!!!!!!
+    }
+
+    switch (cho){
+    case 1:
+        hill_encryption();
+        break;
+    case 2:
+        hill_decryption();
+         
+        break;
+    default:
+        printf("\nInvalid Input!"); //Exits if number given is not 1 or 2
+        exit(0);
+    }
+
+}
+void hill_encryption() {
+    int i, j, k;
+
+    for(i = 0; i < 3; i++){
+            for(k = 0; k < 3; k++){
+                encrypt[i][0] = encrypt[i][0] + c[i][k] * mes[k][0];//mul act with 3x3 key
+            }
+    }
+    printf("\nEncrypted string is: ");
+    for(i = 0; i < 3; i++){
+        printf("%c", (char)(fmod(encrypt[i][0], 26) + 97));//Adds 97 to ascii alphabet word
+    }
+}
+
+void hill_decryption(){
+    int i,j;
+    for (i = 0; i < 3; i++){
+        for (  j= 0; j < 3; j++)
+        {
+            c_ex[i][j]=c[i][j];
+        }
+    }
+    //adding 2 more columns
+    c_ex[0][3]=c[0][0];
+    c_ex[0][4]=c[0][1];
+    c_ex[1][3]=c[1][0];
+    c_ex[1][4]=c[1][1];
+    c_ex[2][3]=c[2][0];
+    c_ex[2][4]=c[2][1];
+
+    //adding 2 more rows
+    c_ex[3][0]=c[0][0];
+    c_ex[3][1]=c[0][1];
+    c_ex[3][2]=c[0][2];
+    c_ex[3][3]=c[0][0];
+    c_ex[3][4]=c[0][1];
+    
+    c_ex[4][0]=c[1][0];
+    c_ex[4][1]=c[1][1];
+    c_ex[4][2]=c[1][2];
+    c_ex[4][3]=c[1][0];
+    c_ex[4][4]=c[1][1];
+
+    /*for ( i = 0; i < 5; i++)
+    {
+        for ( j = 0; j < 5; j++)
+        {
+            printf("%d\t",c_ex[i][j]);
+        }
+        printf("\n");
+    }*/
+
+    for ( i = 0; i < 3; i++){
+        for ( j = 0; j < 3; j++){
+            rev[i][j]=(c_ex[j+1][i+1]* c_ex[j+2][i+2] - c_ex[j+2][i+1] * c_ex[j+1][i+2])%26;
+        }
+    }
+    
+
+    //if number is negative add 26
+    for ( i = 0; i < 3; i++){
+        for ( j = 0; j < 3; j++){
+            if (rev[i][j]<0){
+                rev[i][j]+=26;
+            }
+        }   
+    }
+    /*printf("O antristrofos einai:\n");
+    for ( i = 0; i < 3; i++){
+        for ( j = 0; j < 3; j++){
+            printf("%d\t",rev[i][j]);
+        }
+        printf("\n");
+    }*/
+    i=0;
+    while (flag==0){
+        x = (i * det)%26;
+        if (x==1){
+            flag=1;
+            number=i;
+            //printf("\nDet = %d,We found the inverse k-1 : %d",det,number);
+        }
+        i++;
+    }
+
+    for ( i = 0; i < 3; i++){
+        for ( j = 0; j < 3; j++)
+        {
+            rev[i][j] =(rev[i][j]*number)%26;
+        }
+    }
+    printf("\nWe have the reverse matrix\n");
+    printf("\n-------------------\n");
+    for ( i = 0; i < 3; i++){
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%d\t",rev[i][j]);
+
+        }
+        printf("\n");
+    }
+    printf("-------------------\n");
+
+
+    for(i = 0; i < 3; i++){
+            for(k = 0; k < 3; k++){
+                end[i][0] = end[i][0] + rev[i][k] * mes[k][0];//mul act with 3x3 key
+            }
+        }
+    printf("Decrypted message is: \t");
+     for(i = 0; i < 3; i++){
+        printf("%c",(char)(end[i][0]%26) + 97);
+    }
+
 }
